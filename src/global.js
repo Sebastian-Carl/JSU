73d0bdf483d32280aea48369acc6851dc1d991a8
Carl Aaron Sebastian <carlaaronren20@gmail.com>
2026-03-02 12:23:44 +0800

    Refactored and structuring

---
+++ src/global.js
@@ -0,0 +1,7 @@
+import "./utils/custom/custom.js";
+import "./utils/custom/error/error.js";
+import "./utils/dom/dom.js";
+import "./utils/guards/guards.js";
+import "./utils/primitives/primitives.js";
+import "./utils/storage/storage.js";
+import "./utils/variables.js";
--- src/utils/global.js
+++
@@ -1,7 +0,0 @@
-import "./custom/custom.js";
-import "./custom/error/error.js";
-import "./dom/dom.js";
-import "./guards/guards.js";
-import "./primitives/primitives.js";
-import "./storage/storage.js";
-import "./variables.js";
